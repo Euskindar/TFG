e215e3ef86fe3d47280b16957f3b5e95469a7b25
Jolon <37414389+Euskindar@users.noreply.github.com>
2023-07-26 12:03:29 +0200

    Delete KotlinIntermedio directory

--- KotlinIntermedio/app/src/main/java/com/inicio/kotlinintermedio/Work.kt
+++
@@ -1,8 +0,0 @@
-package com.inicio.kotlinintermedio
-
-// Clases abstractas (no define una implementacion sino un COMPORTAMIENTO)
-abstract class Work {
-
-    // Funcion de clase abstracta (por defecto ya son de tipo OPEN, usables fuera de la clase)
-    abstract fun goToWork()
-}
