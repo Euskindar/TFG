48fdc2f380223f6081070f6f6c53fa83e356d004
julen <i72pehej@gmail.com>
2023-08-03 17:49:51 +0200

    Visualizacion precaria, terminada. Falata colocarlo mas visual.

--- CpuSchedulerApp/app/src/main/java/com/i72pehej/cpuschedulerapp/usecases/results/QueuesScreen.kt
+++ CpuSchedulerApp/app/src/main/java/com/i72pehej/cpuschedulerapp/usecases/results/QueuesScreen.kt
@@ -5,31 +5,22 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.i72pehej.cpuschedulerapp.ui.theme.Azul_com_3
-import com.i72pehej.cpuschedulerapp.ui.theme.Rojo_com_2
-import com.i72pehej.cpuschedulerapp.ui.theme.Verde_deriv_1
 import com.i72pehej.cpuschedulerapp.util.classes.Proceso
 import com.i72pehej.cpuschedulerapp.util.infoResultadosGlobal
 
@@ -48,7 +39,7 @@ fun QueuesScreen() {
         modifier = Modifier
             .fillMaxSize()
             .padding(8.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
+        horizontalAlignment = CenterHorizontally,
         verticalArrangement = Arrangement.Top
     ) {
         TablaColasDeProcesos()
@@ -126,7 +117,7 @@ fun TablaColasDeProcesos() {
                 )
 
                 // Agregamos el numero de columnas correspondientes a cada momento de la linea de tiempos
-                for (nCols in primerIndice until ultimoIndice) {
+                for (nCols in primerIndice..ultimoIndice) {
                     Text(
                         text = "$nCols",
                         modifier = Modifier.weight(1f),
@@ -138,10 +129,8 @@ fun TablaColasDeProcesos() {
         }
 
         // Agregamos una fila para cada cola en cada momento
-
-        items(listaDeColas) {colaActual ->
-
-            // Filas
+        item {
+            // Fila de colas
             Row(modifier = Modifier.padding(4.dp)) {
                 // Agregamos los nombres de los procesos
                 Text(
@@ -158,61 +147,15 @@ fun TablaColasDeProcesos() {
                 )
 
                 // Recorremos las columnas de tiempos
-                for (cols in primerIndice until ultimoIndice) {
-                    // Para cada momento, se compara si el proceso tiene evento y se coloca el simbolo correspondiente
+                for (cols in primerIndice..ultimoIndice) {
                     // Texto a poner en la celda
-                    Text(
-                        text = colaActual.toString(),
-                        fontSize = 15.sp,
-                        color = Color.White,
-                        modifier = Modifier
-                            .align(Alignment.CenterVertically)
-                            .weight(1f),
-                        textAlign = TextAlign.Center
-                    )
+
+                    // TODO -> CREAR UNA FUNCION QUE CREE UNA COLUMNA EN LA QUE CADA CARACTER SE COLOQUE UNO ENCIMA DEL OTRO Y QUEDE EL STRING VERTICAL
+                    // TODO -> OPCION 2: UTILIZAR ITEMS() PARA RECORRER UNO POR UNO LOS CARACTERES Y CREAR UNA FILA PARA EL 1er CARACTER, OTRA PARA EL 2o, etc...
+
+                    Text(text = listaDeColas[cols].toString(), modifier = Modifier.weight(1f))
                 }
             }
         }
-    }
-}
-
-@Composable
-private fun FilaColas() {
-    Row(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceEvenly
-    ) {
-        ItemColaDeProcesos("E", "Espera", Color.Black.copy(alpha = 0.7f))
-        ItemColaDeProcesos("X", "Ejecución", Azul_com_3.copy(alpha = 0.7f))
-        ItemColaDeProcesos("B", "Bloqueado", Rojo_com_2.copy(alpha = 0.7f))
-        ItemColaDeProcesos("C", "Completado", Verde_deriv_1.copy(alpha = 0.7f))
-    }
-}
-
-@Composable
-private fun ItemColaDeProcesos(simbolo: String, etiqueta: String, color: Color) {
-    Row(verticalAlignment = Alignment.CenterVertically) {
-        Surface(
-            modifier = Modifier.size(30.dp),
-            shape = MaterialTheme.shapes.small,
-            color = color
-        ) {
-            Text(
-                text = simbolo,
-                fontSize = 20.sp,
-                color = Color.White,
-                modifier = Modifier.align(Alignment.CenterVertically),
-                textAlign = TextAlign.Center
-            )
-        }
-
-        Spacer(modifier = Modifier.size(4.dp))
-
-        Text(
-            text = etiqueta,
-            fontSize = 12.sp,
-            color = MaterialTheme.colors.onSurface.copy(alpha = 0.8f),
-            modifier = Modifier.align(Alignment.CenterVertically)
-        )
     }
 }
